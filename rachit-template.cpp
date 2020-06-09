2a88d025d67e8d828f10bce383bd5f480a87bfb8
Soubhagyabehera <soubhagya225bdk@gmail.com>
2020-06-09 19:03:28 +0530

    random commit

---
+++ rachit-template.cpp
@@ -0,0 +1,121 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define endl "\n"
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define mk(arr,n,type) type *arr=new type[n]
+#define mii map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi; 
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rng(int lim) {
+	uniform_int_distribution<int> uid(0,lim-1);
+	return uid(rang);
+}
+bool compare(int x,int y)
+{
+	return x>y;
+}
+int mpow(int base, int exp); 
+void ipgraph(int m);
+void dfs(int u, int par);
+
+const int mod = 1'000'000'007;
+const int N = 300, M = N;
+//=======================
+
+vi g[N];
+int a[N][M];
+struct node
+{
+    int val;
+    node(int x):val(x){}
+    int nodefun(int y)
+    {
+        return y+10;
+    }
+};
+int tree[]={39,22,17,13,9,9,8,5,8,6,3,2,7,2,6};
+int sum(int a, int b) {
+    int n=8;
+    a += n; b += n;
+    int s = 0;
+    while (a <= b) {
+    if (a%2 == 1) s += tree[a++];
+    if (b%2 == 0) s += tree[b--];
+    a /= 2; b /= 2;
+    }
+    return s;
+}
+void solve() {
+    cout<<sum(4,7);
+}
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    int t = 1;
+    
+    while(t--) {
+      solve();
+    }
+
+    return 0;
+}
+
+int mpow(int base, int exp) {
+  base %= mod;
+  int result = 1;
+  while (exp > 0) {
+    if (exp & 1) result = ((ll)result * base) % mod;
+    base = ((ll)base * base) % mod;
+    exp >>= 1;
+  }
+  return result;
+}
+
+void ipgraph(int n, int m){
+	int i, u, v;
+	while(m--){
+		cin>>u>>v;
+		g[u-1].pb(v-1);
+		g[v-1].pb(u-1);
+	}
+}
+
+void dfs(int u, int par){
+	for(int v:g[u]){
+		if (v == par) continue;
+		dfs(v, u);
+	}
+}
